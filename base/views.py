dd531653115ad8c666abed68c5b2fd1af715da5b
Abdo Abosamra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-09-01 22:42:39 +0200

    Create Functionality

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView ,UpdateView
 from django.urls import reverse_lazy
 
 from pip._internal.utils.misc import ask
@@ -22,3 +22,9 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks') # this is for redirect after created action.
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')  # this is for redirect after created action.
+
