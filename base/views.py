62ff67330f908abce5665881d036a8a76cf5eb6f
Abdo Abosamra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-09-03 08:51:57 +0200

    Login & Logout

--- base/views.py
+++ base/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView ,UpdateView
+from django.views.generic.edit import CreateView ,UpdateView ,DeleteView
+
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from pip._internal.utils.misc import ask
 
 from .models import Task
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
 
-class TaskList(ListView):
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class TaskList(LoginRequiredMixin ,ListView):
     model = Task
     context_object_name = 'tasks' # we change the name of queryst of objects in our models.
 
@@ -28,3 +39,7 @@ class TaskUpdate(UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')  # this is for redirect after created action.
 
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')  # this is for redirect after Deleted action.
